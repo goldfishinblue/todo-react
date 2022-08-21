d5354c0965171a82da6b89b52842541283e54a05
Mai Trinh <maitrinh.2806@gmail.com>
2022-08-21 20:39:02 +0700

    first cmt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const AddTodo = ({ addTodo }) => {
+  const [title, setTitle] = React.useState('')
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (!title) {
+      return
+    }
+    const todo = {
+      title,
+      isCompleted: false,
+      id: uuidv4(),
+      date: Date.now()
+    }
+    addTodo(todo)
+    setTitle('')
+  }
+return (
+    <form className="form" onSubmit={handleSubmit}>
+      <input 
+        className="form__input" 
+        type="text" 
+        name="title"
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+      />
+      <button className="form__button" type="submit">ADD</button>
+    </form>
+  )
 }
 
+const Todos = ({ todos, completeTodo, deleteTodo }) => {  
+  return (
+    <ul className="todos">
+      {todos.map((todo, i) => (
+        <li 
+          className={todo.isCompleted ? 'todos__item completed' : 'todos__item'} 
+          key={i}
+        >
+          <box-icon 
+            onClick={() => completeTodo(todo.id)}
+            name={todo.isCompleted ? 'checkbox-checked' : 'checkbox'}
+            color="white"  
+          />
+          <span>{todo.title}</span>
+          <box-icon 
+            name="trash-alt" 
+            color="white" 
+            onClick={() => deleteTodo(todo.id)}
+          />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+const App = () => {
+  const [todos, setTodos] = React.useState([])
+  
+  const addTodo = todo => setTodos([...todos, todo])
+  
+  const completeTodo = id => {
+    const newTodos = todos.map(todo => {
+      return todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    })
+    setTodos(newTodos)
+  }
+  
+  const deleteTodo = id => setTodos(todos.filter(todo => todo.id !== id))
+  
+  return (
+    <div className="container">
+      <h1 className="title">Todo List</h1>
+      <AddTodo addTodo={addTodo} />
+      <Todos todos={todos} completeTodo={completeTodo} deleteTodo={deleteTodo} />
+    </div>
+  )
+}
 export default App;
+
+// todoState = {
+//   content: 'asdf',
+//   
+// }
